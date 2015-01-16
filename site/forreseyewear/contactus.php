a85199fcc5fea5f90aeedb5cb763e9a9811d1623
TirtaWR <tirtawening@gmail.com>
2015-01-17 00:09:17 +0700

    bikin landing page, maintenance page, contant us, dan warranty

---
+++ site/forreseyewear/contactus.php
@@ -0,0 +1,32 @@
+<?php require "header.php"; ?>
+
+	<div class="main">
+   	  <div class="about">
+   	    <div class="container">
+   	    
+   	    	<div class="row" style="margin-top:40px;">
+		   	  	 <div class="col-md-8 col-lg-offset-2 about_left">
+		   	  	 	<h3>Contact Us</h3>
+		   	  	 	<p class="m_1" style="text-align:justify">
+		   	  	 		If you have any questions, comments, advice or feedback, don’t hesitate to contact us! 
+		   	  	 		<br>We are exist in a lot of platform such as LINE , Facebook , Twitter , Instagram  , or direct message , only to give you the best service ever.
+		   	  	 	</p>
+		   	  	 	<h4 style="margin-top: 10px;"><b>OPERATION HOURS</b></h4>
+		   	  	    <p class="m_1" style="text-align:justify">Serve you everyday 08.00-22.00 WIB and closed on major holidays.</p>
+		   	  	    <h4 style="margin-top: 10px;"><b>CALL US</b></h4>
+		   	  	    <p class="m_1" style="text-align:justify">
+		   	  	    	Phone number : 081xxxxxx <br>
+		   	  	    	Line : Forres_ <br>
+		   	  	    	Instagram : Forres_ <br> 
+		   	  	    	Facebook : Forres_ <br>
+		   	  	    	Twitter : ForresEyewear</p>
+		   	  	    <h4 style="margin-top: 10px;"><b>EMAIL</b></h4>
+		   	  	    <p class="m_1" style="text-align:justify">hello@forreseyewear.com</p>
+
+		   	  	 </div>
+			</div>
+   	    </div>
+   	  </div>
+   	</div>
+
+<?php require "footer.php"; ?>	
---
+++ site/forreseyewear/warranty.php
@@ -0,0 +1,39 @@
+<?php require "header.php"; ?>
+
+	<div class="main">
+   	  <div class="about">
+   	    <div class="container">
+   	    
+   	    	<div class="row" style="margin-top:40px;">
+		   	  	 <div class="col-md-8 col-lg-offset-2 about_left">
+		   	  	 	<h3>Warranty</h3>
+		   	  	 	<p class="m_1" style="text-align:justify">
+		   	  	 		Forres Eyewear are constructed using the high quality materials and workmanship that goes into the elegant design and product. Nevertheless, we just human that can do a mistake that resulted defect product during process or delivery.</p>
+		   	  	 	<p class="m_1" style="text-align:justify"><br>Forres Eyewear warrants products against manufacturing defects in materials and workmanship for a period of one year (365 days) from the date of purchase. It requires proof of purchase from Forres purchase to qualify products as warrantable.  Forres Eyewear will review your claim and repair or replace the item at no cost to you.</p>
+		   	  	 	<p class="m_1" style="text-align:justify"><br>All claims must be submitted directly via email to warranty@forreseyewear.com</p>
+		   	  	 	<p class="m_1" style="text-align:justify"><b>The following steps for warranty :</b></p>
+		   	  	 	<ul>
+		   	  	 		<li>Fill claim form and proof of purchase requirement in our website </li>
+		   	  	 		<li>Picture your broken glasses </li>
+		   	  	 		<li>Send it to warranty@forreseyewear.com</li>
+		   	  	 	</ul>
+		   	  	 	<p class="m_1" style="text-align:justify"><b>The following steps of Proof of Purchase :</b></p>
+		   	  	 	<ul>
+		   	  	 		<li>Your Forres Eyewear order number.</li>
+		   	  	 		<li>Picture or scan of the physical receipt and warranty card with a visible date of purchase , customer name , and eyewear model</li>
+		   	  	 		<li>Picture of your bank statement with a visible purchase date.</li>
+		   	  	 	</ul>
+		   	  	 	<h4 style="margin-top: 10px;"><br><b>COVERED BY WARRANTY:</b></h4>
+		   	  	 	<p class="m_1" style="text-align:justify">
+		   	  	 		<b>POPPED LENS</b><br>Popped lens without signs of impact or force.<br>
+		   	  	 		<b>BROKEN HINGE</b><br>Broken hinges with no signs of impact or force.<br>
+		   	  	 		<b>BROKEN IN DELIVERY PROCESS</b><br>Damage to item upon delivery; Claim must be filed within 24 hours of receipt.
+		   	  	 	</p>
+
+		   	  	 </div>
+			</div>
+   	    </div>
+   	  </div>
+   	</div>
+
+<?php require "footer.php"; ?>	
