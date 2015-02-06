4a9409db8beb8a56afc43befec2b475945302a85
Tegar <tegarnization@gmail.com>
2015-02-06 22:07:21 +0700

    adding comingsoon page

---
+++ site/forreseyewear/comingsoon.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<link href='http://fonts.googleapis.com/css?family=Open+Sans:100,200,300,400,500,600,700,800,900' rel='stylesheet' type='text/css'>
+<head>
+	<meta charset="UTF-8">
+	<div style="text-align: center;">
+		<img src="images/logoforres-comingsoon.png" alt="">
+	</div>
+	<title>Forreseyewear | Coming Soon</title>
+</head>
+<body>
+	<h1 style="text-align: center; font-family: 'Open Sans', sans-serif;">
+		Coming soon..
+	</h1>
+</body>
+</html>
